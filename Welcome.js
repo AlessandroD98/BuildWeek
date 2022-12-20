bdc4986b5be819bfb4782e48fd7d8be06c9aaf0e
AlessandroD98 <alessandro.comunicazioni@gmali.com>
2022-12-20 18:24:47 +0100

    linktestpage

--- Welcome.js
+++ Welcome.js
@@ -1,8 +1,8 @@
 window.addEventListener(
   "DOMContentLoaded",
   function (e) {
-    var myForm = document.getElementById("example4");
-    var checkForm = function (e) {
+    let myForm = document.getElementById("form");
+    let checkForm = function (e) {
       if (!this.terms.checked) {
         alert("MMM... If you are a real Epicoders please accepts our terms!");
         this.terms.focus();
@@ -14,8 +14,8 @@ window.addEventListener(
     // attach the form submit handler
     myForm.addEventListener("submit", checkForm, false);
 
-    var myCheckbox = document.getElementById("field_terms");
-    var myCheckboxMsg = "MMM... If you are a real Epicoders please accepts our terms!";
+    let myCheckbox = document.getElementById("field_terms");
+    let myCheckboxMsg = "MMM... If you are a real Epicoders please accepts our terms!";
 
     // set the starting error message
     myCheckbox.setCustomValidity(myCheckboxMsg);
