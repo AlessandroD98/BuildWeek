d8c9577e35981fb6d33b39a509c563eb41267d8f
AlessandroD98 <alessandro.comunicazioni@gmali.com>
2022-12-20 18:09:40 +0100

    cambiobottone

--- Welcome.js
+++ Welcome.js
@@ -1,1 +1,33 @@
-a;
+window.addEventListener(
+  "DOMContentLoaded",
+  function (e) {
+    var myForm = document.getElementById("example4");
+    var checkForm = function (e) {
+      if (!this.terms.checked) {
+        alert("MMM... If you are a real Epicoders please accepts our terms!");
+        this.terms.focus();
+        e.preventDefault(); // equivalent to return false
+        return;
+      }
+    };
+
+    // attach the form submit handler
+    myForm.addEventListener("submit", checkForm, false);
+
+    var myCheckbox = document.getElementById("field_terms");
+    var myCheckboxMsg = "MMM... If you are a real Epicoders please accepts our terms!";
+
+    // set the starting error message
+    myCheckbox.setCustomValidity(myCheckboxMsg);
+
+    // attach checkbox handler to toggle error message
+    myCheckbox.addEventListener(
+      "change",
+      function (e) {
+        this.setCustomValidity(this.validity.valueMissing ? myCheckboxMsg : "");
+      },
+      false
+    );
+  },
+  false
+);
