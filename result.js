f85c7af860fc4bd7f5b03af9fc7b00974c5b6aac
Giacomo <giax.95@live.it>
2022-12-23 10:46:21 +0100

    Update

--- result.js
+++ result.js
@@ -13,9 +13,9 @@ const myChart = new Chart(ctx, {
       {
         label: "# of Votes",
         data: [primoParametro, secondoParametro],
-        backgroundColor: ["#00FFFF", "#d20094"],
+        backgroundColor: ["rgba(0, 255, 255, 0.6)", "rgba(210, 0, 148, 0.6)"],
         borderColor: ["#000000", "#000000"],
-        borderWidth: 4,
+        borderWidth: 2,
       },
     ],
   },
@@ -23,45 +23,78 @@ const myChart = new Chart(ctx, {
     responsive: true,
     maintainAspectRatio: false,
     legend: {
-      display: false,
+      display: true,
       position: "right",
+      labels: {
+        boxWidth: 20,
+        generateLabels: function (chart) {
+          const data = chart.data;
+          if (data.labels.length && data.datasets.length) {
+            return data.labels.map(function (label, i) {
+              const meta = chart.getDatasetMeta(0);
+              const ds = data.datasets[0];
+              const arc = meta.data[i];
+              const custom = (arc && arc.custom) || {};
+              const getValueAtIndexOrDefault = Chart.helpers.getValueAtIndexOrDefault;
+              const arcOpts = chart.options.elements.arc;
+              const fill = custom.backgroundColor
+                ? custom.backgroundColor
+                : getValueAtIndexOrDefault(ds.backgroundColor, i, arcOpts.backgroundColor);
+              const stroke = custom.borderColor
+                ? custom.borderColor
+                : getValueAtIndexOrDefault(ds.borderColor, i, arcOpts.borderColor);
+              const bw = custom.borderWidth
+                ? custom.borderWidth
+                : getValueAtIndexOrDefault(ds.borderWidth, i, arcOpts.borderWidth);
+
+              return {
+                text: label + ": " + ds.data[i] + "/" + max,
+                fillStyle: fill,
+                strokeStyle: stroke,
+                lineWidth: bw,
+                hidden: isNaN(ds.data[i]) || meta.data[i].hidden,
+                index: i,
+              };
+            });
+          }
+          return [];
+        },
+      },
     },
   },
 });
 const canvas = document.getElementById("myChart");
-canvas.style.width = "80%";
-canvas.style.height = "80%";
+canvas.style.width = "300%";
+canvas.style.height = "300%";
 
-const latosx = function () {
-  const newprimoparametro = (primoParametro / 10) * 100;
-  const rispotecorr = document.getElementById("rispcorr");
-  const pcorr = document.createElement("p");
-  pcorr.innerText = "Correct";
-  const p2corr = document.createElement("p");
-  p2corr.innerText = newprimoparametro + "%";
-  const p3corr = document.createElement("p");
-  p3corr.innerText = primoParametro + "/10 questions";
-  pcorr.classList = "correct";
-  p2corr.classList = "correct";
-  p3corr.classList = "correct";
-  rispotecorr.appendChild(pcorr);
-  rispotecorr.appendChild(p2corr);
-  rispotecorr.appendChild(p3corr);
-};
+// const latosx = function () {
+//   const newprimoparametro = (primoParametro / 10) * 100;
+//   const rispotecorr = document.getElementById("rispcorr");
+//   const pcorr = document.createElement("p");
+//   pcorr.innerText = "Correct";
+//   const p2corr = document.createElement("p");
+//   p2corr.innerText = newprimoparametro + "%";
+//   const p3corr = document.createElement("p");
+//   p3corr.innerText = primoParametro + "/10 questions";
+//   pcorr.classList = "correct";
+//   p2corr.classList = "correct";
+//   p3corr.classList = "correct";
+//   rispotecorr.appendChild(pcorr);
+//   rispotecorr.appendChild(p2corr);
+//   rispotecorr.appendChild(p3corr);
 
-const latodx = function () {
-  const newsecondoparametro = (secondoParametro / 10) * 100;
-  const rispoteerr = document.getElementById("risperr");
-  const perr = document.createElement("p");
-  perr.innerText = "Wrong";
-  const p2err = document.createElement("p");
-  p2err.innerText = newsecondoparametro + "%";
-  const p3err = document.createElement("p");
-  p3err.innerText = secondoParametro + "/10 questions";
-  perr.classList = "wrong";
-  p2err.classList = "wrong";
-  p3err.classList = "wrong";
-  rispoteerr.appendChild(perr);
-  rispoteerr.appendChild(p2err);
-  rispoteerr.appendChild(p3err);
-};
+//   const newsecondoparametro = (secondoParametro / 10) * 100;
+//   const rispoteerr = document.getElementById("risperr");
+//   const perr = document.createElement("p");
+//   perr.innerText = "Wrong";
+//   const p2err = document.createElement("p");
+//   p2err.innerText = newsecondoparametro + "%";
+//   const p3err = document.createElement("p");
+//   p3err.innerText = secondoParametro + "/10 questions";
+//   perr.classList = "wrong";
+//   p2err.classList = "wrong";
+//   p3err.classList = "wrong";
+//   rispoteerr.appendChild(perr);
+//   rispoteerr.appendChild(p2err);
+//   rispoteerr.appendChild(p3err);
+// };
